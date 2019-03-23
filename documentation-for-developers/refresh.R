88d9e9e2243cf66cdad29f01be7b6bf25e4e88c2
Will Beasley <wibeasley@hotmail.com>
2019-03-23 00:21:38 -0500

    pkgdown website
    ref #2

--- documentation-for-developers/refresh.R
+++ documentation-for-developers/refresh.R
@@ -8,7 +8,7 @@ devtools::check_man() #Should return NULL
 devtools::build_vignettes()
 pkgdown::clean_site()
 pkgdown::build_site()
-system("R CMD Rd2pdf --no-preview --force --output=./documentation-peek.pdf ." )
+# system("R CMD Rd2pdf --no-preview --force --output=./documentation-peek.pdf ." )
 
 devtools::run_examples(); #dev.off() #This overwrites the NAMESPACE file too
 # devtools::run_examples(, "table-nih-enrollment.Rd")
